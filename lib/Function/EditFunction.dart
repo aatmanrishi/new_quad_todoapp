5cd552bac077415f0e1eed88b9c37c58d836ac2f
aatmanrishi <itzrrrrishabh@gmail.com>
2025-02-17 17:54:43 +0530

    Added the Edit Functionality

--- lib/Function/EditFunction.dart
+++ lib/Function/EditFunction.dart
@@ -1,4 +1,4 @@
-
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:new_quad_todoapp/UiController/UiController.dart';
 
@@ -17,13 +17,12 @@ List<String> getTaskStepsData() {
 }
 
 void initiateEditFunction(String uiId) {
-  print('Here is the uniquie Id of object $uiId');
+  print('Here is the unique Id of object $uiId');
   uiController.isEdit.value = true;
 
+  // Iterate through the completeData to find the matching task and copy data
   for (int i = 0; i < uiController.completeData.length; i++) {
     if (uiController.completeData[i].uiId == uiId) {
-      //  Add values to fields of there data
-      // Will edited item data to controllers from completeList
       uiController.tempUiId.value = uiId;
       uiController.taskHeading.value.text =
           uiController.completeData[i].taskHeading;
@@ -37,13 +36,18 @@ void initiateEditFunction(String uiId) {
           uiController.completeData[i].reminderTime;
       uiController.tempUiIdAssociatedStaus.value =
           uiController.completeData[i].taskStaus;
-      uiController.tempIsImportantValue =
-          uiController.completeData[i].isImportant.value;
-      // uiController.temptaskSteps.value = uiController.completeData[i].taskSteps;
+      uiController.tempIsImportantValue.value =
+          uiController.completeData[i].isImportant;
+
+      // Now, push each of the task steps into tempTask (temptaskSteps)
+      uiController.temptaskSteps.clear(); // Clear previous steps
+      for (var step in uiController.completeData[i].taskSteps) {
+        uiController.temptaskSteps.add(TextEditingController(text: step));
+      }
     }
   }
 
-  //
+  // Repeat for tasksData if the task wasn't found in completeData
   for (int i = 0; i < uiController.tasksData.length; i++) {
     if (uiController.tasksData[i].uiId == uiId) {
       uiController.tempUiId.value = uiId;
@@ -57,14 +61,23 @@ void initiateEditFunction(String uiId) {
       uiController.reminderTime.value = uiController.tasksData[i].reminderTime;
       uiController.tempUiIdAssociatedStaus.value =
           uiController.tasksData[i].taskStaus;
-      uiController.tempIsImportantValue =
-          uiController.tasksData[i].isImportant.value;
-      // uiController.temptaskSteps.value = uiX;
+      uiController.tempIsImportantValue.value =
+          uiController.tasksData[i].isImportant;
+
+      // Now, push each of the task steps into tempTask (temptaskSteps)
+      uiController.temptaskSteps.clear(); // Clear previous steps
+      for (var step in uiController.tasksData[i].taskSteps) {
+        uiController.temptaskSteps.add(TextEditingController(text: step));
+      }
     }
   }
 }
 
 void finalEditFunction() {
+  // Get the task steps from tempTask (temptaskSteps)
+  var taskSteps = getTaskStepsData();
+
+  // Create a new Task object with updated information
   var task = Task(
     uiId: uiController.tempUiId.value,
     taskHeading: uiController.taskHeading.value.text,
@@ -75,11 +88,13 @@ void finalEditFunction() {
     reminderTime: uiController.reminderTime.value,
     taskDaysRepeated: uiController.daysIndex,
     isImportant: uiController.tempIsImportantValue.value,
-    taskSteps: [],
+    taskSteps: taskSteps, // Updated taskSteps
   );
+
+  // Update the task in completeData
   for (int i = 0; i < uiController.completeData.length; i++) {
     if (uiController.completeData[i].uiId == uiController.tempUiId.value) {
-      print("Every thing goin good");
+      print("Updating task in completeData");
       uiController.completeData.removeAt(i);
       uiController.completeData.insert(i, task);
       uiController.tempUiId.value = '';
@@ -87,9 +102,10 @@ void finalEditFunction() {
     }
   }
 
+  // Update the task in tasksData
   for (int i = 0; i < uiController.tasksData.length; i++) {
     if (uiController.tasksData[i].uiId == uiController.tempUiId.value) {
-      print("Everything going good");
+      print("Updating task in tasksData");
       uiController.tasksData.removeAt(i);
       uiController.tasksData.insert(i, task);
       uiController.tempUiId.value = '';
@@ -97,6 +113,7 @@ void finalEditFunction() {
     }
   }
 
+  // Clear the temp task data after finalizing the edit
   uiController.taskHeading.value.clear();
   uiController.taskDetails.value.clear();
   uiController.daysIndex.clear();
@@ -107,7 +124,8 @@ void finalEditFunction() {
   uiController.isBottomSheath.value = false;
   uiController.tempIsImportantValue.value = false;
   uiController.temptaskSteps.clear();
-  // Closing ui ELements
+
+  // Closing UI elements
   uiController.isCalendar.value = false;
   uiController.isRepeat.value = false;
   uiController.isReminder.value = false;
